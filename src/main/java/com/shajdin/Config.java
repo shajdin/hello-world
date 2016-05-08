bce92809c2f293796f1d3de8bad87affd3e1c8d7
shajdin <sergej.hajdin@yahoo.com>
2016-05-08 02:00:13 +0200

    trying to run on heroku

--- src/main/java/com/shajdin/Config.java
+++ src/main/java/com/shajdin/Config.java
@@ -1,10 +1,14 @@
 package com.shajdin;
 
+import javax.sql.DataSource;
+
+import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.shajdin.service.UserService;
@@ -17,6 +21,11 @@ import com.shajdin.service.UserServiceImpl;
 @ComponentScan(basePackages={"com.shajdin.service", "com.shajdin.conntroller"})
 public class Config {
 	
+//	@Bean
+//	public DataSource dataSource(){
+//		return new EmbeddedDatabaseBuilder().build();
+//	}
+//	
 	@Bean
 	public UserService userService(){
 		return new UserServiceImpl();
